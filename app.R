0fd0d273689860d1ef618b9efd8b83a7d2037935
B Pope <poper38@gmail.com>
2020-04-07 23:08:19 -0400

    Formatting for graph; added various options for graph, including log scale, relative time measure, and metric selection; added notification icon showing data date range;

--- app.R
+++ app.R
@@ -5,13 +5,35 @@ library(httr)
 library(shiny)
 library(shinydashboard)
 
+# Function to convert variable names to display names
+Proper<-function(x){
+    translate<-NULL
+    translate<-state.name
+    names(translate)<-state.abb
+    translate<-c(translate,
+                 "positive"="Total Confirmed Cases",
+                 "negative"="Total Negative Tests",
+                 "death"="Total Deaths",
+                 "recovered"="Total Recovered",
+                 "positiveincrease"="New Confirmed Cases",
+                 "negativeincrease"="New Negative Tests",
+                 "deathincrease"="New Deaths",
+                 "date"="Date",
+                 "days_since"="Days Since First Case"
+                 )
+    sapply(x,function(y){
+        if(is.na(translate[tolower(y)]))y
+        else translate[tolower(y)]
+    })
+}
 
 # UI ####
 ui <- dashboardPage(
 
     # Application title
     dashboardHeader(
-        title="Corona Virus Tracker"
+        title="Corona Virus Tracker",
+        dropdownMenuOutput("date_notif")
     ),
 
     # Sidebar for filtering
@@ -29,7 +51,26 @@ ui <- dashboardPage(
                     tableOutput("corona_table")
             ),
             tabItem("charts",
-                    plotOutput("corona_trend")
+                    plotOutput("corona_trend"),
+                    fluidRow(
+                        column(2,radioButtons("scale_box",
+                                     "Choose Scale:",
+                                     choices = c("Standard","Log"),
+                                     selected="Standard")),
+                        column(2,radioButtons("time_box",
+                                              "Select Time measure:",
+                                              choices=c("Absolute"="date","Relative"="days_since"))),
+                        column(8,radioButtons("metric_box",
+                                     "Choose metric:",
+                                     choices=c("Total Confirmed Cases"="positive",
+                                               "Total Negative Tests"="negative",
+                                               "Total Deaths"="death",
+                                               "Total Recovered"="recovered",
+                                               "New Confirmed Cases"="positiveIncrease",
+                                               "New Negative Tests"="negativeIncrease",
+                                               "New Deaths"="deathIncrease"),
+                                     selected="positive"))
+                    )
             )
         )
     )
@@ -38,15 +79,32 @@ ui <- dashboardPage(
 
 # SERVER ####
 server <- function(input, output,session) {
-    # 1. Query API ####
+    # 1. Query API and format data ####
     path <- "https://covidtracking.com/api/v1/states/daily.json"
     request <- GET(url = path
     )
     response <- content(request, as = "text", encoding = "UTF-8")
-    df <- fromJSON(response, flatten = TRUE) %>%
-        data.frame()
-    df$state_name<-state.name[match(df$state,state.abb)]
+    df <- fromJSON(response, flatten = TRUE) %>% data.frame()
+    
+    abbs<-c(state.abb,'AS','DC','GU','MP','PR','VI')
+    s_names<-c(state.name,
+               'American Samoa',
+               'District of Columbia',
+               'Guam',
+               'Northern Mariana Islands',
+               'Puerto Rico',
+               'US Virgin Islands')
+    df$state_name<-s_names[match(df$state,abbs)]
+    
     df$date<-as.Date(as.character(df$date),format="%Y%m%d")
+    
+    first_date<-df %>% 
+        filter(positive>0) %>% 
+        group_by(state) %>% 
+        summarize(first_date=min(date))
+    df<-df %>% left_join(first_date) %>% 
+        mutate(days_since=as.integer(date-first_date))
+    
     # 2. Filters for user ####
     output$state_filter<-renderUI({
         selectInput("state_box",
@@ -57,6 +115,21 @@ server <- function(input, output,session) {
         
     })
     
+    # 2. Create Date notification ####
+    output$date_notif <- renderMenu({
+        dropdownMenu(
+            type = "notifications",
+            icon = icon("calendar"),
+            badgeStatus = NULL,
+            headerText = "Data date range:",
+            notificationItem(HTML(paste0(
+                min(df$date, na.rm = T),
+                "<br>through<br>",
+                max(df$date, na.rm = T)
+            )),
+            icon = icon(NULL))
+        )
+    })
     
     # 3. Create totals table ####
     output$corona_table <- renderTable({
@@ -68,21 +141,37 @@ server <- function(input, output,session) {
             ungroup() %>%
             summarise_at(
                 vars(positive,
+                     negative,
                      death,
                      recovered),
                 sum,
                 na.rm = TRUE
             ) %>% 
             rename("Total Confirmed Cases"="positive",
+                   "Total Negative Tests"="negative",
                    "Total Deaths"="death",
                    "Total Recovered"="recovered")
     })
     
+    # 4. Create trend chart
     output$corona_trend<-renderPlot({
         filter_data<-df %>%
             filter(state_name %in% input$state_box |
-                       is.null(input$state_box))
-        ggplot(filter_data) +geom_line(aes(date,positive,group=state_name))
+                       is.null(input$state_box)) %>% 
+            mutate(value=!!sym(input$metric_box),
+                   time=!!sym(input$time_box))
+        
+        p<-ggplot(filter_data) +geom_line(aes(time,value,color=state_name)) +
+            labs(x=Proper(input$time_box),
+                 y=Proper(input$metric_box),
+                 title=paste(Proper(input$metric_box),"over Time"),
+                 color="State")
+        
+        if(input$scale_box=="Log"){
+            p + 
+                scale_y_log10()
+        }
+        else p
     })
 }
 
